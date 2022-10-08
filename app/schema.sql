bfe0f8f244eb67ee11ef344b9adfb62161e33b6d
equipo7mintic <equipo7mintic@gmail.com>
2022-10-07 23:09:10 -0500

    Funtional Proyect

--- app/schema.sql
+++ app/schema.sql
@@ -52,4 +52,4 @@ CREATE TABLE credentials(
   password TEXT NOT NULL
 );
 
-INSERT INTO credentials (name,user,password) VALUES ('EMAIL_APP','developmentcapstone', '$TR41NC0URS3R4$');
+INSERT INTO credentials (name,user,password) VALUES ('EMAIL_APP','scesara@uninorte.edu.co', 'kamuss_07');
